3344e57437222902a8fffe0095f2c20a2e50379a
Edgar Reyes <erk5530@gmail.com>
2022-11-20 20:51:33 -0600

    Updating Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,31 @@
 import '@/assets/css/Login.css'
+import React from 'react'
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext'
+import { Password } from 'primereact/password'
 
 export const Login = () => {
-  return <span>Login</span>
+  return (
+    <>
+      <form className='login'>
+        <div className='text-center'>
+            <h3 className='fw-light mb-3'>Bienvenido al panel de administraci&oacute;n</h3>
+        </div>
+        <div className='p-inputgroup mb-3'>
+          <span className='p-inputgroup-addon'>
+            <i className='pi pi-user'></i>
+          </span>
+          <InputText placeholder='Escriba su usuario' />
+        </div>
+
+        <div className='p-inputgroup mb-3'>
+          <Password placeholder='Ingrese su contrase&ntilde;a' feedback={false} toggleMask/>
+          <span className='p-inputgroup-addon'>
+            <i className='pi pi-key'></i>
+          </span>
+        </div>
+        <Button label='Login' icon='pi pi-angle-right' iconPos='right' className="p-button"/>
+      </form>
+    </>
+  )
 }
